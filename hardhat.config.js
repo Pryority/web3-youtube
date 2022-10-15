65361169fc6ba9f9291aa47c79d07ef63b2d89fd
pryority <matthewapryor@gmail.com>
2022-10-14 21:33:56 -0400

    deploy mumbai testnet contract to 0xA3CeDb57CaB24FCCe267f7612F8d625F856bF8F7

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    localhost: {
+      chainId: 31337
+    },
+    mumbai: {
+      networkId: 80001,
+      url: "https://rpc-mumbai.maticvigil.com/",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  paths: {
+    artifacts: './artifacts'
+  }
 };
